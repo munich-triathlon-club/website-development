7cf85df67c363927d696b3452ed748b11228f03f
schaeferto <tobias.schaefer.12@googlemail.com>
2024-04-14 22:35:54 +0200

    Add navbar

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  addNavClickHandler("nav-icon-header");
+  addNavClickHandler("nav-icon-menu");
+  addBackDropClickHandler();
+});
+
+function addNavClickHandler(id) {
+    document.getElementById(id).addEventListener("click", function () {
+        document.getElementById("navbar").classList.toggle("open");
+
+        // Toggle backdrop
+        document.getElementById("backdrop").classList.toggle("open");
+      });
+}
+
+function addBackDropClickHandler() {
+    document.getElementById("backdrop").addEventListener("click", function () {
+        document.getElementById("navbar").classList.remove("open");
+        document.getElementById("backdrop").classList.remove("open");
+      });
+}
