ad738529f36950247222352c5f4d1d05d831c0ad
schaeferto <tobias.schaefer.12@googlemail.com>
2024-04-16 21:42:30 +0200

    Activate simple scoll only on mobile

--- static/js/main.js
+++ static/js/main.js
@@ -2,20 +2,26 @@ document.addEventListener("DOMContentLoaded", function () {
   addNavClickHandler("nav-icon-header");
   addNavClickHandler("nav-icon-menu");
   addBackDropClickHandler();
+  removeSimpleScrollOnMobile();
 });
 
 function addNavClickHandler(id) {
-    document.getElementById(id).addEventListener("click", function () {
-        document.getElementById("navbar").classList.toggle("open");
-
-        // Toggle backdrop
-        document.getElementById("backdrop").classList.toggle("open");
-      });
+  document.getElementById(id).addEventListener("click", function () {
+    document.getElementById("navbar").classList.toggle("open");
+    document.getElementById("backdrop").classList.toggle("open");
+  });
 }
 
 function addBackDropClickHandler() {
-    document.getElementById("backdrop").addEventListener("click", function () {
-        document.getElementById("navbar").classList.remove("open");
-        document.getElementById("backdrop").classList.remove("open");
-      });
+  document.getElementById("backdrop").addEventListener("click", function () {
+    document.getElementById("navbar").classList.remove("open");
+    document.getElementById("backdrop").classList.remove("open");
+  });
+}
+
+function removeSimpleScrollOnMobile() {
+  if(window.innerWidth >= 768) {
+    const scrollContainer = document.getElementById("scroll-container")
+    SimpleScrollbar.initEl(scrollContainer);
+  }
 }
