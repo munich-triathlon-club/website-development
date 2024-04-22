d3096e7c21490112b66ec7e3c024fff952f6e908
schaeferto <tobias.schaefer.12@googlemail.com>
2024-04-22 21:02:26 +0200

    Only activate simple-scrollbar on desktop

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   addNavClickHandler("nav-icon-header");
   addNavClickHandler("nav-icon-menu");
   addBackDropClickHandler();
+  removeSimpleScrollOnMobile();
 });
 
 function addNavClickHandler(id) {
@@ -16,4 +17,11 @@ function addBackDropClickHandler() {
     document.getElementById("navbar").classList.remove("open");
     document.getElementById("backdrop").classList.remove("open");
   });
+}
+
+function removeSimpleScrollOnMobile() {
+  if(window.innerWidth >= 768) {
+    const scrollContainer = document.getElementById("scroll-container")
+    SimpleScrollbar.initEl(scrollContainer);
+  }
 }
